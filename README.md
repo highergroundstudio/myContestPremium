f185d35dca444c576848103d119dc853d9d5021a
Kyle King <highergroundstudio@users.noreply.github.com>
2024-08-07 11:09:23 -0700

    Move over from other repo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# myContestPremium
+myContest
+=========
+
+Docs for myContest Premium Wordpress Contest plugin
+
+[Download the docs for offline viewing](https://github.com/highergroundstudio/myContest/archive/gh-pages.zip)
---
+++ assets/js/app.js
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) 2007-2013 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * @author Ariel Flesler
+ * @version 1.4.5
+ */
+;(function($){var h=$.scrollTo=function(a,b,c){$(window).scrollTo(a,b,c)};h.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:true};h.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(e,f,g){if(typeof f=='object'){g=f;f=0}if(typeof g=='function')g={onAfter:g};if(e=='max')e=9e9;g=$.extend({},h.defaults,g);f=f||g.duration;g.queue=g.queue&&g.axis.length>1;if(g.queue)f/=2;g.offset=both(g.offset);g.over=both(g.over);return this._scrollable().each(function(){if(e==null)return;var d=this,$elem=$(d),targ=e,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}$.each(g.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=h.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(g.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=g.offset[pos]||0;if(g.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*g.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(g.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&g.queue){if(old!=attr[key])animate(g.onAfterFirst);delete attr[key]}});animate(g.onAfter);function animate(a){$elem.animate(attr,f,g.easing,a&&function(){a.call(this,e,g)})}}).end()};h.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+/*
+* jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/ 
+*/
+jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});
+
+/*!
+ * Documenter 1.6
+ * http://rxa.li/documenter
+ *
+ * Copyright 2011, Xaver Birsak
+ * http://revaxarts.com
+ *
+ */
+//if Cufon replace headings
+if(typeof Cufon == 'function') Cufon.replace('h1, h2, h3, h4, h5, h6');
+ 
+$(document).ready(function() {
+	var timeout,
+		sections = new Array(),
+		sectionscount = 0,
+		win = $(window),
+		sidebar = $('#documenter_sidebar'),
+		nav = $('#documenter_nav'),
+		logo = $('#documenter_logo'),
+		navanchors = nav.find('a'),
+		timeoffset = 50,
+		hash = location.hash || null;
+		iDeviceNotOS4 = (navigator.userAgent.match(/iphone|ipod|ipad/i) && !navigator.userAgent.match(/OS 5/i)) || false,
+		badIE = $('html').prop('class').match(/ie(6|7|8)/)|| false;
+		
+	//handle external links (new window)
+	$('a[href^=http]').bind('click',function(){
+		window.open($(this).attr('href'));
+		return false;
+	});
+	
+	//IE 8 and lower doesn't like the smooth pagescroll
+	if(!badIE){
+		window.scroll(0,0);
+		
+		$('a[href^=#]').bind('click touchstart',function(){
+			hash = $(this).attr('href');
+			$.scrollTo.window().queue([]).stop();
+			goTo(hash);
+			return false;
+		});
+		
+		//if a hash is set => go to it
+		if(hash){
+			setTimeout(function(){
+				goTo(hash);
+			},500);
+		}
+	}
+	
+	
+	//We need the position of each section until the full page with all images is loaded
+	win.bind('load',function(){
+		
+		var sectionselector = 'section';
+		
+		//Documentation has subcategories		
+		if(nav.find('ol').length){
+			sectionselector = 'section, h4';
+		}
+		//saving some information
+		$(sectionselector).each(function(i,e){
+			var _this = $(this);
+			var p = {
+				id: this.id,
+				pos: _this.offset().top
+			};
+			sections.push(p);
+		});
+		
+		
+		//iPhone, iPod and iPad don't trigger the scroll event
+		if(iDeviceNotOS4){
+			nav.find('a').bind('click',function(){
+				setTimeout(function(){
+					win.trigger('scroll');				
+				},duration);
+				
+			});
+			//scroll to top
+			window.scroll(0,0);
+		}
+
+		//how many sections
+		sectionscount = sections.length;
+		
+		//bind the handler to the scroll event
+		win.bind('scroll',function(event){
+			clearInterval(timeout);
+			//should occur with a delay
+			timeout = setTimeout(function(){
+				//get the position from the very top in all browsers
+				pos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+				
+				//iDeviceNotOS4s don't know the fixed property so we fake it
+				if(iDeviceNotOS4){
+					sidebar.css({height:document.height});
+					logo.css({'margin-top':pos});
+				}
+				//activate Nav element at the current position
+				activateNav(pos);
+			},timeoffset);
+		}).trigger('scroll');
+
+	});
+	
+	//the function is called when the hash changes
+	function hashchange(){
+		goTo(location.hash, false);
+	}
+	
+	//scroll to a section and set the hash
+	function goTo(hash,changehash){
+		win.unbind('hashchange', hashchange);
+		hash = hash.replace(/!\//,'');
+		win.stop().scrollTo(hash,duration,{
+			easing:easing,
+			axis:'y'			
+		});
+		if(changehash !== false){
+			var l = location;
+			location.href = (l.protocol+'//'+l.host+l.pathname+'#!/'+hash.substr(1));
+		}
+		win.bind('hashchange', hashchange);
+	}
+	
+	
+	//activate current nav element
+	function activateNav(pos){
+		var offset = 100,
+		current, next, parent, isSub, hasSub;
+		win.unbind('hashchange', hashchange);
+		for(var i=sectionscount;i>0;i--){
+			if(sections[i-1].pos <= pos+offset){
+				navanchors.removeClass('current');
+				current = navanchors.eq(i-1);
+				current.addClass('current');
+				
+				parent = current.parent().parent();
+				next = current.next();
+				
+				hasSub = next.is('ol');
+				isSub = !parent.is('#documenter_nav');
+				
+				nav.find('ol:visible').not(parent).slideUp('fast');
+				if(isSub){
+					parent.prev().addClass('current');
+					parent.stop().slideDown('fast');
+				}else if(hasSub){
+					next.stop().slideDown('fast');
+				}
+				win.bind('hashchange', hashchange);
+				break;
+			};
+		}	
+	}
+	
+	
+});
